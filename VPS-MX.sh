cbae6618f9bfb56838c9fd1a8aed6b18519ab272
phreaker56 <92488891+phreaker56@users.noreply.github.com>
2022-01-21 09:38:28 -0800

    Update VPS-MX.sh

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -181,7 +181,7 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 [[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
 }  
 function_verify () {
-  permited=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Control/master/Control-IP")
+  permited=$(curl -sSL "https://raw.githubusercontent.com/phreaker56/VPS-MX-8.0/master/Control-IP")
   [[ $(echo $permited|grep "${IP}") = "" ]] && {
   echo -e "\n\n\n\033[1;95m======================================================\n ¡ESTA KEY NO CONCUERDA CON EL INSTALADOR!,CONATACTE A @Rufu99\n======================================================\n"
   [[ -d /etc/newadm ]] && rm -rf /etc/newadm
